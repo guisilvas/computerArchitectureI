de7ef686341b48a2bf6e7c30ced285015714e09f
guisilvas <guilhermesoaressilvadev@gmail.com>
2024-10-01 23:58:57 -0300

    [update] Verilog

---
+++ Verilog/001.v
@@ -0,0 +1,14 @@
+// CI - Standard logic
+// Full-Custom IC (uses transistor)
+//FPGA
+
+// Gates : AND, OR, XOR, NOT
+// Muxes/Demuxes
+// Registers/Memory
+// Aithmetic Operations (Adder, Sbtracter, Multiplier, etc)
+// State Machines
+// Any combinations of Above
+
+module
+
+end module
